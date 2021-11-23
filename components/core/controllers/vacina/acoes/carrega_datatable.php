256172a189ecb816fe968a65841d35ca16202a46
samuelsolutio <samuel.becker@solutiosoftware.com.br>
2021-11-22 21:21:48 -0300

    correção perdi as contas

--- components/core/controllers/vacina/acoes/carrega_datatable.php
+++ components/core/controllers/vacina/acoes/carrega_datatable.php
@@ -8,12 +8,13 @@ $vacinas = $dao_vacina->seleciona_todos();
 $retorno['data'] = array();
 if($vacinas){
     foreach($vacinas as $vacina){
-        $data = explode('-', $vacina->get_validade());
+        $data = $vacina->get_validade() ? explode('-', $vacina->get_validade()) : '';
+        $validade = $data ? $data[2] . "/" . $data[1] . "/" . $data[0] : '';
         array_push($retorno['data'], array(
             'id' => $vacina->get_id(),
             'lote' => $vacina->get_lote(),
             'descricao' => $vacina->get_descricao(),
-            'validade' => $data[2] . "/" . $data[1] . "/" . $data[0],
+            'validade' => $validade,
             'fabricante' => $vacina->get_fabricante(),
             'dose' => $vacina->get_dose(),
             'quantidade' => $vacina->get_quantidade(),
